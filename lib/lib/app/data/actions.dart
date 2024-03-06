fa2918c09f6bbfc5616ac48b7cf27c8b1b330046
ouknik <oopokni@gmail.com>
2024-03-06 20:36:57 +0100

    flutter_launcher_icons

--- lib/lib/app/data/actions.dart
+++
@@ -1,8 +0,0 @@
-class Actionjson {
-  static var actions = [
-    {"action": "Validate", "icon": "validate"},
-    {"action": "View", "icon": "view"},
-    {"action": "Edit", "icon": "edit"},
-    {"action": "Delete", "icon": "delete"},
-  ];
-}
